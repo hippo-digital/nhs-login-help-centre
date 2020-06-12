81f94ae7050e95f3b56e44375c674bd5fa8944f3
tomhalson <tomhalson@users.noreply.github.com>
2020-06-12 12:08:28 +0000

    deploy: 5f4ad7bce1f6b7f48d9a665103214adc4df96e7a

--- js/search_data.js
+++ js/search_data.js
@@ -235,7 +235,7 @@ var tipuesearch = {
 			{
 				"url": "/manage/delete#how-to-manage-your-nhs-login-within-websites-and-apps",
 				"title": "How to manage your NHS login within websites and apps",
-				"text": "You should be able to manage your NHS login settings from within the websites and apps that use NHS login. Each website and app you visit may offer a different way for you to access your NHS login settings. Contact the providers of the website or app you are using if you cannot find a way to access your NHS login settings. ***",
+				"text": "You should be able to manage your NHS login settings from within the websites and apps that use NHS login. Each website and app you visit may offer a different way for you to access your NHS login settings. Contact the providers of the website or app you are using if you cannot find a way to access your NHS login settings. ---",
 				"tags": "",
 				"hasSearchExcerpt": false
 			}
@@ -243,7 +243,7 @@ var tipuesearch = {
 			{
 				"url": "/manage/delete#managing-nhs-login-from-the-nhs-app",
 				"title": "Managing NHS login from the NHS App",
-				"text": "At present there is no option to manage your NHS login settings within the NHS App. This is something we are currently working towards. However, you can do this by selecting this link: https://settings.login.nhs.uk/. For security reasons, you will need to log in with your usual NHS login details before you can change any of your details. ***",
+				"text": "At present there is no option to manage your NHS login settings within the NHS App. This is something we are currently working towards. However, you can do this by selecting this link: https://settings.login.nhs.uk/. For security reasons, you will need to log in with your usual NHS login details before you can change any of your details. ---",
 				"tags": "",
 				"hasSearchExcerpt": false
 			}
@@ -251,7 +251,7 @@ var tipuesearch = {
 			{
 				"url": "/manage/delete#how-to-delete-nhs-login",
 				"title": "How to delete NHS login",
-				"text": "You can delete your NHS login by visiting your NHS login settings. Scroll down to the bottom and select ‘delete NHS login’. You will be asked to enter your password to confirm. Once you delete your NHS login, you will not be able to use it to access any websites or apps. If you are trying to delete your account with just one website or app in particular, you should contact them directly. You may then still use your NHS login to access other websites and apps.",
+				"text": "You can delete your NHS login by visiting your NHS login settings. Scroll down to the bottom and select ‘delete NHS login’. You will be asked to enter your password to confirm. Once you delete your NHS login, you will not be able to use it to access any websites or apps. If you are trying to delete your account with just one website or app in particular, you should contact them directly. You may then still use your NHS login to access other websites and apps. If you delete your NHS login, any websites or apps you have used with NHS login may keep some information about you in line with their terms and conditions and privacy notice. We may also keep some information about you in line with NHS login&#39;s privacy notice",
 				"tags": "",
 				"hasSearchExcerpt": false
 			}
